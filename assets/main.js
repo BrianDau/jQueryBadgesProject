8c48f40d4ae1523276d35b1e167a9ee2b1b96673
BrianDau <brian@the-ds.com>
2016-12-28 21:55:30 -0500

    Adding badge code

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,33 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/BlueStarBrian.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      var $badges = $('#badges');
+      console.log(response.courses.completed);
+      response.courses.completed.forEach(function(course) {
+        var $course = $('<div />', {
+          'class': 'course'
+        }).appendTo($badges);
 
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($course);
+
+        $('<img />', {
+          src: course.badge
+        }).appendTo($course);
+
+        $('<a />', {
+          href: course.url,
+          target: '_blank',
+          'class': 'btn btn-primary',
+          text: 'See Course'
+        }).appendTo($course);
+      })
+
+    }
+  });
 });
